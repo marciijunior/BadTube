94dc69de7baee0b2e88f6fdc49594f3e29624ab3
Marcio <marciojsjunior.mj@gmail.com>
2025-06-26 02:28:04 -0300

    espero que seja a ultima

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ import { criarHeader } from './components/header.js';
 import { criarMenuLateral } from './components/menuLateral.js';
 import { criarSecaoCategorias } from './components/categorias.js';
 import { criarListaVideos, adicionarFiltros } from './components/listaVideos.js';
-import { videos } from './videos.js'; // Importa o array com os vídeos
+import { videos } from './videos.js';
 
 const app = document.getElementById("app");
 
@@ -15,14 +15,11 @@ menuWrapper.className = "menu__container-wrapper";
 menuWrapper.appendChild(criarMenuLateral());
 app.appendChild(menuWrapper);
 
-// Seção de categorias
+// Categorias
 app.appendChild(criarSecaoCategorias());
 
-// Pega o array de vídeos dentro do objeto na posição 0
-const listaVideos = videos[0].videos;
+// Lista de vídeos
+app.appendChild(criarListaVideos(videos));
 
-// Cria a lista de vídeos passando o array correto
-app.appendChild(criarListaVideos(listaVideos));
-
-// Ativa os filtros para funcionar a pesquisa e categoria
+// Filtros
 adicionarFiltros();
